b83da5083c01d7868689aae8b0442b54a64e29aa
AItEKS <egorka.ku06@mail.ru>
2025-03-04 07:33:54 +0000

    Added new procedure, CTE, indexes, view

--- Tasks/db_maker.sql
+++ Tasks/db_maker.sql
@@ -46,6 +46,9 @@ BEGIN
 				REFERENCES egor.military_positions (military_positions_id)
 		);
 
+		CREATE INDEX IF NOT EXISTS idx_users_military_positions_id ON egor.users (military_positions_id);
+
+
 		INSERT INTO egor.users (first_name, last_name, military_positions_id) 
 		VALUES ('Иван', 'Иванов', 1), ('Петр', 'Петров', 2), ('Сергей', 'Сергеев', 3), ('Алексей', 'Алексеев', 4), ('Дмитрий', 'Дмитриев', 5);
 		
@@ -90,6 +93,9 @@ BEGIN
 				REFERENCES egor.users (user_id)
 		);
 
+		CREATE INDEX IF NOT EXISTS idx_measurment_batch_user_id ON egor.measurment_batch (user_id);
+
+
 		INSERT INTO egor.measurment_batch (start_period, position_x, position_y, user_id)
 		VALUES ('2025-01-31 10:20:00', 6.5, 3.5, 1), ('2025-01-31 10:29:00', 7.2, 4.5, 2);
 
@@ -122,6 +128,16 @@ BEGIN
 	            REFERENCES egor.measurment_batch (measurment_batch_id)
 	    );
 
+		CREATE INDEX IF NOT EXISTS idx_measurment_params_measurment_type_id ON egor.measurment_params (measurment_type_id);
+		CREATE INDEX IF NOT EXISTS idx_measurment_params_measurment_batch_id ON egor.measurment_params (measurment_batch_id);
+		CREATE INDEX IF NOT EXISTS idx_measurment_params_height ON egor.measurment_params (height);
+		CREATE INDEX IF NOT EXISTS idx_measurment_params_temperature ON egor.measurment_params (temperature);
+		CREATE INDEX IF NOT EXISTS idx_measurment_params_pressure ON egor.measurment_params (pressure);
+		CREATE INDEX IF NOT EXISTS idx_measurment_params_wind_speed ON egor.measurment_params (wind_speed);
+		CREATE INDEX IF NOT EXISTS idx_measurment_params_wind_direction ON egor.measurment_params (wind_direction);
+		CREATE INDEX IF NOT EXISTS idx_measurment_params_bullet_speed ON egor.measurment_params (bullet_speed);
+
+
     	RAISE NOTICE 'Table "measurment_params" created.';
 	
 	ELSE
@@ -138,6 +154,8 @@ BEGIN
             max_value NUMERIC(8,2),
 			units CHARACTER VARYING(100)
         );
+
+		CREATE INDEX IF NOT EXISTS idx_measure_settings_const_name ON egor.measure_settings (const_name);
 
         INSERT INTO egor.measure_settings (const_name, min_value, max_value, units) 
 		VALUES ('temperature', -58, 58, '°C'), ('pressure', 500, 900, 'mmHg'), ('wind_direction', 0, 59, ''), 
@@ -157,6 +175,8 @@ BEGIN
             temperature NUMERIC(4,2),
             delta NUMERIC(4,2)
         );
+
+		CREATE INDEX IF NOT EXISTS idx_constant_params_1_temperature ON egor.constant_params_1 (temperature);
 
         INSERT INTO egor.constant_params_1 (temperature, delta) 
 		VALUES (-1, 0), (0, 0.5), (5, 0.5), (10, 1), (15, 1), (20, 1.5), (25, 2), (30, 3.5), (40, 4.5);
@@ -179,6 +199,10 @@ BEGIN
 		    is_positive BOOLEAN NOT NULL,
 		    const_values INTEGER[] NOT NULL
         );
+
+		CREATE INDEX IF NOT EXISTS idx_constant_params_2_height ON egor.constant_params_2 (height);
+		CREATE INDEX IF NOT EXISTS idx_constant_params_2_is_positive ON egor.constant_params_2 (is_positive);
+
 
         INSERT INTO egor.constant_params_2 (height, is_positive, const_values) 
 		VALUES (200, FALSE, ARRAY[-1, -2, -3, -4, -5, -6, -7, -8, -9, -20, -29, -39, -49]), 
@@ -206,8 +230,72 @@ BEGIN
         RAISE NOTICE 'Table "constant_params_2" already exists.';
     
 	END IF;
+
+
+	----- constant_params_3_dmk table -----
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'egor' AND tablename = 'constant_params_3_dmk') THEN
+		CREATE SEQUENCE IF NOT EXISTS egor.constant_params_3_dmk_seq START 1 INCREMENT BY 1;
+		
+		CREATE TABLE egor.constant_params_3_dmk (
+		    id INTEGER NOT NULL DEFAULT nextval('egor.constant_params_3_dmk_seq'::regclass),
+		    height INTEGER NOT NULL,
+		    const_values INTEGER[] NOT NULL,
+			alphay NUMERIC(4, 2) NOT NULL
+        );
+
+		CREATE INDEX IF NOT EXISTS idx_constant_params_3_dmk_height ON egor.constant_params_3_dmk (height);
+
+        INSERT INTO egor.constant_params_3_dmk (height, const_values, alphay) 
+		VALUES (200, ARRAY[4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22], 1), 
+				(400, ARRAY[5, 7, 10, 11, 12, 14, 17, 18, 20, 22, 23, 25, 27], 2),
+				(800, ARRAY[5, 8, 10, 11, 13, 15, 18, 19, 21, 23, 25, 27, 28], 3),
+				(1200, ARRAY[5, 8, 11, 12, 13, 16, 19, 20, 22, 24, 26, 28, 30], 3),
+				(1600, ARRAY[6, 8, 11, 13, 14, 17, 20, 21, 23, 25, 27, 29, 32], 4),
+				(2000, ARRAY[6, 9, 11, 13, 14, 17, 20, 21, 24, 26, 28, 30, 32], 4),
+				(2400, ARRAY[6, 9, 12, 14, 15, 18, 21, 22, 25, 27, 29, 32, 34], 4),
+				(3000, ARRAY[6, 9, 12, 14, 15, 18, 21, 23, 25, 28, 30, 32, 36], 5),
+				(4000, ARRAY[6, 10, 12, 14, 16, 19, 22, 24, 26, 29, 32, 34, 36], 5);
+
+        RAISE NOTICE 'Table "constant_params_3_dmk" created.';
+		
+    ELSE
+        RAISE NOTICE 'Table "constant_params_3_dmk" already exists.';
+    
+	END IF;
 	
 
+	----- constant_params_3_bp table -----
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'egor' AND tablename = 'constant_params_3_bp') THEN
+		CREATE SEQUENCE IF NOT EXISTS egor.constant_params_3_bp_seq START 1 INCREMENT BY 1;
+		
+		CREATE TABLE egor.constant_params_3_bp (
+		    id INTEGER NOT NULL DEFAULT nextval('egor.constant_params_3_bp_seq'::regclass),
+		    height INTEGER NOT NULL,
+		    const_values INTEGER[] NOT NULL,
+			alphay NUMERIC(4, 2) NOT NULL
+        );
+
+		CREATE INDEX IF NOT EXISTS idx_constant_params_3_bp_height ON egor.constant_params_3_bp (height);
+
+        INSERT INTO egor.constant_params_3_bp (height, const_values, alphay) 
+		VALUES (200, ARRAY[3, 4, 5, 6, 7, 7, 8, 9, 10, 11, 12, 12], 0), 
+				(400, ARRAY[4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 1),
+				(800, ARRAY[4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16], 2),
+				(1200, ARRAY[4, 5, 7, 8, 8, 9, 11, 12, 13, 15, 15, 16], 2),
+				(1600, ARRAY[4, 6, 7, 8, 9, 10, 11, 13, 14, 15, 17, 17], 3),
+				(2000, ARRAY[4, 6, 7, 8, 9, 10, 11, 13, 14, 16, 17, 18], 3),
+				(2400, ARRAY[4, 6, 8, 9, 9, 10, 12, 14, 15, 16, 18, 19], 3),
+				(3000, ARRAY[5, 6, 8, 9, 10, 11, 12, 14, 15, 17, 18, 19], 4),
+				(4000, ARRAY[5, 6, 8, 9, 10, 11, 12, 14, 16, 18, 19, 20], 4);
+
+        RAISE NOTICE 'Table "constant_params_3_bp" created.';
+		
+    ELSE
+        RAISE NOTICE 'Table "constant_params_3_bp" already exists.';
+    
+	END IF;
+	
+	
 	----- interpolation_params -----
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'interpolation_params') THEN
         CREATE TYPE egor.interpolation_params AS
@@ -518,6 +606,65 @@ BEGIN
 	        result_array[array_index] := current_result;
 	        array_index := array_index + 1;
 	    END LOOP;
+	END;
+	$BODY$;
+
+
+	------ pd_get_avg_air_speed() ------
+	CREATE OR REPLACE PROCEDURE egor.pd_get_avg_air_speed(
+	    var_speed NUMERIC(4, 2),
+	    measurement_type INTEGER,
+	    OUT result_array1 INTEGER[],
+	    OUT result_array2 NUMERIC[]
+	)
+	LANGUAGE 'plpgsql'
+	AS $BODY$
+	DECLARE
+	    row_data RECORD;
+	    index_value INTEGER;
+	    min_val INTEGER;
+	    max_val INTEGER;
+		coeff NUMERIC(4, 2);
+	    table_name TEXT;
+	BEGIN
+	    IF measurement_type = 1 THEN
+	        min_val := 3;
+	        max_val := 15;
+	        table_name := 'constant_params_3_dmk';
+			coeff := 1;
+	    ELSE
+	        min_val := 40;
+	        max_val := 150;
+	        table_name := 'constant_params_3_bp';
+			coeff := 10;
+	    END IF;
+	
+	    IF var_speed < min_val THEN
+	        result_array1 := ARRAY[0, 0, 0, 0, 0, 0, 0, 0];
+	        result_array2 := ARRAY[0, 0, 0, 0, 0, 0, 0, 0];
+	    ELSIF var_speed BETWEEN min_val AND max_val THEN
+	        result_array1 := ARRAY[]::INTEGER[];
+	        result_array2 := ARRAY[]::NUMERIC[];
+	        FOR row_data IN EXECUTE format('SELECT * FROM egor.%I ORDER BY height', table_name)
+	        LOOP
+	            index_value := ROUND((var_speed - min_val) / coeff);
+				
+	            IF index_value >= 0 AND index_value < array_length(row_data.const_values, 1) THEN
+	                result_array1 := array_append(result_array1, row_data.const_values[index_value + 1]); 
+	            ELSE
+	                result_array1 := array_append(result_array1, NULL);
+	            END IF;
+	            result_array2 := array_append(result_array2, var_speed + row_data.alphay);
+	        END LOOP;
+	    ELSE
+	        result_array1 := ARRAY[]::INTEGER[];
+	        result_array2 := ARRAY[]::NUMERIC[];
+	        FOR row_data IN EXECUTE format('SELECT * FROM egor.%I ORDER BY height', table_name)
+	        LOOP
+	            result_array1 := array_append(result_array1, (SELECT MAX(value) FROM unnest(row_data.const_values) AS value));
+	            result_array2 := array_append(result_array2, var_speed + row_data.alphay);
+	        END LOOP;
+	    END IF;
 	END;
 	$BODY$;
 
